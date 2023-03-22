994b7f91729951a1d8876543f9a5fa141f6cf47d
chris <chrissunny94@gmail.com>
2023-03-22 10:32:51 +0530

    updated install script

--- install_and_build_simulator_stuff.sh
+++ install_and_build_simulator_stuff.sh
@@ -5,13 +5,15 @@ sudo apt install ros-melodic-gazebo-ros -y
 sudo apt install ros-melodic-jackal* -y
 sudo apt install ros-melodic-microstain* -y
 
-sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
-wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
-sudo apt update
+#sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+#wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+#sudo apt update
 sudo apt upgrade -y
 
 sudo apt-get install ros-melodic-slam-gmapping -y
-sudo apt-get install ros-kinetic-teleop-twist-keyboard -y
+sudo apt-get install ros-melodic-teleop-twist-keyboard -y
+sudo apt-get install ros-melodic-controller-manager -y
+sudo apt-get install ros-melodic-velodyne* -y
 
 source /opt/ros/melodic/setup.bash
 catkin_make
